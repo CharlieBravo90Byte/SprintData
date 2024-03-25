979d68aac3a82d8fc18bba16bff1705bfa5fc327
CharlieBravo90Byte <159206815+CharlieBravo90Byte@users.noreply.github.com>
2024-03-25 18:43:30 -0300

    parte 2 para los entity

--- src/main/java/cl/charlie/pizzeria/persitence/entity/OrderItemEntity.java
+++ src/main/java/cl/charlie/pizzeria/persitence/entity/OrderItemEntity.java
@@ -1,4 +1,35 @@
 package cl.charlie.pizzeria.persitence.entity;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.IdClass;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "order_item")
+@IdClass(OrderItemId.class)
+@Getter
+@Setter
+@NoArgsConstructor
 public class OrderItemEntity {
+
+    private Integer idOrder;
+
+    private Integer idItem;
+
+    private Integer idPizza;
+
+    private Double quantity;
+
+    private Double price;
+
+    private OrderEntity order;
+
+    private PizzaEntity pizza;
+
+
+
 }
