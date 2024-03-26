8689542084633d3be8ebdfb7aa96d28781849ef2
CharlieBravo90Byte <159206815+CharlieBravo90Byte@users.noreply.github.com>
2024-03-26 09:13:23 -0300

    entity definidas

--- src/main/java/cl/charlie/pizzeria/persitence/entity/OrderItemEntity.java
+++ src/main/java/cl/charlie/pizzeria/persitence/entity/OrderItemEntity.java
@@ -1,9 +1,7 @@
 package cl.charlie.pizzeria.persitence.entity;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.IdClass;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -16,18 +14,29 @@ import lombok.Setter;
 @NoArgsConstructor
 public class OrderItemEntity {
 
+    @Id
+    @Column(name = "id_order", nullable = false)
     private Integer idOrder;
 
+    @Id
+    @Column(name = "id_item", nullable = false)
     private Integer idItem;
 
+    @Column(name = "id_pizza", nullable = false)
     private Integer idPizza;
 
+    @Column(nullable = false, columnDefinition = "Decimal(2,1)")
     private Double quantity;
 
+    @Column( nullable = false, columnDefinition = "Decimal(5,2)")
     private Double price;
 
+    @ManyToOne
+    @JoinColumn(name = "id_order", referencedColumnName = "id_order", insertable = false, updatable = false)
     private OrderEntity order;
 
+    @OneToOne
+    @JoinColumn(name = "id_pizza", referencedColumnName = "id_pizza", insertable = false, updatable = false)
     private PizzaEntity pizza;
 
 
